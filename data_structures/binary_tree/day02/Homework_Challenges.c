697671ca4a0e4ea69b455bb8ed06971717ef7441
zakaria lazrak <z.zakarialazrak@gmail.com>
2023-01-24 16:51:28 +0100

    {}

--- data_structures/binary_tree/day02/Homework_Challenges.c
+++ data_structures/binary_tree/day02/Homework_Challenges.c
@@ -12,14 +12,58 @@ int	ft_tree_max(node *current) /* positive nums only */
 	int mx = ft_max(ft_tree_max(current->left), ft_tree_max(current->right));
 	return (ft_max(mx, current->data));
 }
-/*
-int	ft_tree_height(node *current)
+
+int	ft_tree_height(node *current, int h)
 {
- 	if (!current)
- 		return (0);
- 	int	left = 1 + ft_tree_height(current->left);
+	int	l;
+	int	r;
+
+	l = 0;
+	r = 0;
+	if (current->left)
+		l = ft_tree_height(current->left, h + 1);
+	else
+		l = h;
+ 	if (current->right)
+		r = ft_tree_height(current->right, h + 1);
+	else
+		r = h;
+	return(ft_max(l,r));
 }
-*/
+
+int	ft_count_all_nodes(node *current, int n)
+{
+	int	l;
+	int	r;
+
+	if (current->left)
+		n += 1 + ft_count_all_nodes(current->left, 0);
+	if (current->right)
+		n += 1 + ft_count_all_nodes(current->right, 0);
+	return (n);
+}
+
+int	ft_count_leaf_nodes(node *current, int n)
+{
+	if (!current->left && !current->right)
+		return (1);
+	if (current->left)
+		n += ft_count_leaf_nodes(current->left, 0);
+	if (current->right)
+		n += ft_count_leaf_nodes(current->right, 0);
+	return (n);
+}
+
+int	ft_search(node *current, int value)
+{
+	if (!current)
+		return (0);
+	if (current->data == value)
+		return (1);
+	int	n = ft_search(current->left, value);
+	int nn = ft_search(current->right, value);
+	return (n + nn);
+}
 
 int	main()
 {
@@ -29,12 +73,24 @@ int	main()
 	int		arr1[] = {2, 41, 8};
 	int		arr2[] = {2, 5, 9};
 	int 	arr3[] = {3, 6, 10};
+	//int 	arr4[] = {3, 6, 10, 98};
+	int 	arr4[] = {3, 6,66};
+
+
 
 	ft_add(root, arr,  "LLL", 3);
 	ft_add(root, arr1, "LLR", 3);
 	ft_add(root, arr2, "LRR", 3);
 	ft_add(root, arr3, "RRL", 3);
+	ft_add(root, arr4, "RRR", 3);
 
-	//print_inorder(root);
+	print_inorder(root);
+	printf("\n");
 	printf("max = %d\n", ft_tree_max(root));
+	printf("height = %d\n", ft_tree_height(root, 0));
+	printf("num of nodes = %d\n", ft_count_all_nodes(root, 1));
+	printf("num of leaf nodes = %d\n", ft_count_leaf_nodes(root, 0));
+	printf("is exist ? --> %d\n", ft_search(root, 7));
+	printf("is exist ? --> %d\n", ft_search(root, 70));
+
 }
