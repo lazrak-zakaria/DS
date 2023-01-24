589fd7ca4af09c392eead7e0537964f0206c8dd2
zakaria lazrak <z.zakarialazrak@gmail.com>
2023-01-24 17:41:26 +0100

    {}

--- data_structures/binary_tree/day02/Homework_Challenges.c
+++ data_structures/binary_tree/day02/Homework_Challenges.c
@@ -5,15 +5,45 @@ int	ft_max(int a, int b)
 	return (a > b ? a : b);
 }
 
-int	ft_tree_max(node *current) /* positive nums only */
+// int	ft_tree_max(node *current) /* positive nums only */
+// {
+// 	if (!current)
+// 		return (-1);
+// 	int mx = ft_max(ft_tree_max(current->left), ft_tree_max(current->right));
+// 	return (ft_max(mx, current->data));
+// }
+
+int	ft_tree_max(node *current)
 {
-	if (!current)
-		return (-1);
-	int mx = ft_max(ft_tree_max(current->left), ft_tree_max(current->right));
-	return (ft_max(mx, current->data));
+	int	ans;
+
+	ans = current->data;
+	if (current->left)
+		ans = ft_max(ans, ft_tree_max(current->left));
+	if (current->right)
+		ans = ft_max(ans, ft_tree_max(current->right));
+	return (ans);
 }
 
-int	ft_tree_height(node *current, int h)
+// int	ft_tree_height(node *current, int h)
+// {
+// 	int	l;
+// 	int	r;
+
+// 	l = 0;
+// 	r = 0;
+// 	if (current->left)
+// 		l = ft_tree_height(current->left, h + 1);
+// 	else
+// 		l = h;
+//  	if (current->right)
+// 		r = ft_tree_height(current->right, h + 1);
+// 	else
+// 		r = h;
+// 	return(ft_max(l,r));
+// }
+
+int	ft_tree_height(node *current)
 {
 	int	l;
 	int	r;
@@ -21,36 +51,47 @@ int	ft_tree_height(node *current, int h)
 	l = 0;
 	r = 0;
 	if (current->left)
-		l = ft_tree_height(current->left, h + 1);
-	else
-		l = h;
+		l = 1 + ft_tree_height(current->left);
+	if (current->right)
+		r = 1 + ft_tree_height(current->right);
+	return ft_max(l, r);
+}
+
+// int	ft_count_all_nodes(node *current, int n)
+// {
+// 	int	l;
+// 	int	r;
+
+// 	if (current->left)
+// 		n += 1 + ft_count_all_nodes(current->left, 0);
+// 	if (current->right)
+// 		n += 1 + ft_count_all_nodes(current->right, 0);
+// 	return (n);
+// }
+
+int	ft_count_all_nodes(node *current)
+{
+	int	n;
+
+	n = 1;
+	if (current->left)
+ 		n += ft_count_all_nodes(current->left);
  	if (current->right)
-		r = ft_tree_height(current->right, h + 1);
-	else
-		r = h;
-	return(ft_max(l,r));
+ 		n += ft_count_all_nodes(current->right);
+ 	return (n);
 }
 
-int	ft_count_all_nodes(node *current, int n)
+int	ft_count_leaf_nodes(node *current)
 {
-	int	l;
-	int	r;
+	int	n;
 
-	if (current->left)
-		n += 1 + ft_count_all_nodes(current->left, 0);
-	if (current->right)
-		n += 1 + ft_count_all_nodes(current->right, 0);
-	return (n);
-}
-
-int	ft_count_leaf_nodes(node *current, int n)
-{
+	n = 0;
 	if (!current->left && !current->right)
-		return (1);
+		n = 1;
 	if (current->left)
-		n += ft_count_leaf_nodes(current->left, 0);
+		n += ft_count_leaf_nodes(current->left);
 	if (current->right)
-		n += ft_count_leaf_nodes(current->right, 0);
+		n += ft_count_leaf_nodes(current->right);
 	return (n);
 }
 
@@ -71,10 +112,10 @@ int	main()
 
 	int		arr[]  = {2, 41, 7};
 	int		arr1[] = {2, 41, 8};
-	int		arr2[] = {2, 5, 9};
+	int		arr2[] = {2, 5, -98};
 	int 	arr3[] = {3, 6, 10};
-	//int 	arr4[] = {3, 6, 10, 98};
-	int 	arr4[] = {3, 6,66};
+	int 	arr4[] = {3, 6, 10, 98};
+	//int 	arr4[] = {3, 6,66};
 
 
 
@@ -82,14 +123,14 @@ int	main()
 	ft_add(root, arr1, "LLR", 3);
 	ft_add(root, arr2, "LRR", 3);
 	ft_add(root, arr3, "RRL", 3);
-	ft_add(root, arr4, "RRR", 3);
+	ft_add(root, arr4, "RRLL", 4);
 
 	print_inorder(root);
 	printf("\n");
 	printf("max = %d\n", ft_tree_max(root));
-	printf("height = %d\n", ft_tree_height(root, 0));
-	printf("num of nodes = %d\n", ft_count_all_nodes(root, 1));
-	printf("num of leaf nodes = %d\n", ft_count_leaf_nodes(root, 0));
+	printf("height = %d\n", ft_tree_height(root));
+	printf("num of nodes = %d\n", ft_count_all_nodes(root));
+	printf("num of leaf nodes = %d\n", ft_count_leaf_nodes(root));
 	printf("is exist ? --> %d\n", ft_search(root, 7));
 	printf("is exist ? --> %d\n", ft_search(root, 70));
 
